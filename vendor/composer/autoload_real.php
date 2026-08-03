e940c11bec72701bb7c38c9ca6ad6178d5bb0034
Sabuj Kundu <manchumahara@gmail.com>
2026-08-03 17:18:26 +0600

    composer updated

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -31,7 +31,6 @@ class ComposerAutoloaderInit24cc889104b9659635f28495edc3f608
         require __DIR__ . '/autoload_static.php';
         call_user_func(\Composer\Autoload\ComposerStaticInit24cc889104b9659635f28495edc3f608::getInitializer($loader));
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit24cc889104b9659635f28495edc3f608::$files;
